7942ec634564d710df3b8b2187148569ab8192f9
Chris Wright <chriswrighty1192@gmail.com>
2020-02-06 13:05:27 +0000

    remove repetitive css

--- src/Components/app-header.js
+++ src/Components/app-header.js
@@ -3,19 +3,15 @@ import "./app-header.css";
 
 const AppHeader = () => {
   return (
-    <div className="app-header">
-      <header className="rental-car-header">
-        <img
-          className="rental-image"
-          src="https://cdn2.rcstatic.com/com.rentalcars.185492029745.eu-west-1.web.prod.static-live/images/header/logo_white.svg"
-          alt="Car Hire - Rentalcars.com"
-        ></img>
-      </header>
-      <div className="line">{""}</div>
-      <div className="hero">
-        <h1 className="car-hire">Car Hire - Search, Compare & Save</h1>
-      </div>
-    </div>
+    <header className="app-header">
+      <img
+        className="rental-image"
+        src="https://cdn2.rcstatic.com/com.rentalcars.185492029745.eu-west-1.web.prod.static-live/images/header/logo_white.svg"
+        alt="Car Hire - Rentalcars.com"
+      ></img>
+      <div className="line-seperator" />
+      <h1 className="car-hire-title">Car Hire - Search, Compare & Save</h1>
+    </header>
   );
 };
 
